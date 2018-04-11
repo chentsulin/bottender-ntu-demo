a3ac5abeb6513f03682f877860d12087fb0d2c90
C.T. Lin <chentsulin@gmail.com>
2018-04-11 15:17:09 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { ConsoleBot } = require('bottender');
+
+const bot = new ConsoleBot();
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+bot.createRuntime();
